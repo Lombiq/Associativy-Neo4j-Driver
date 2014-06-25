17c0477e3928b5fba2ed412a43fb0225de7cd957
Piedone <piedone@pyrocenter.hu>
2014-06-25 23:15:07 +0200

    Refactoring dynamic pages

---
+++ Handlers/AdminPageHandler.cs
@@ -0,0 +1,29 @@
+﻿using Associativy.Administration;
+using Associativy.Administration.Models.Pages.Admin;
+using Associativy.Neo4j.Models.Pages.Admin;
+using Associativy.Neo4j.Services;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Environment.Extensions;
+using Piedone.HelpfulLibraries.Contents.DynamicPages;
+
+namespace Associativy.Neo4j.Handlers
+{
+    [OrchardFeature("Associativy.Neo4j.Administration")]
+    public class AdminPageHandler : ContentHandler
+    {
+        protected override void Initializing(InitializingContentContext context)
+        {
+            var pageContext = context.PageContext();
+
+            if (pageContext.Group != AdministrationPageConfigs.Group) return;
+
+            if (!(pageContext.Page.As<AssociativyManageGraphPart>().GraphDescriptor.Services.ConnectionManager is INeo4jConnectionManager)) return;
+
+            if (pageContext.Page.IsPage("ManageGraph", pageContext.Group))
+            {
+                pageContext.Page.Weld(new AssociativyNeo4jManageGraphPart());
+            }
+        }
+    }
+}
